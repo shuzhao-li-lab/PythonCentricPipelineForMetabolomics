935cf082fee2af6a50221261eebfa44c20075900
Joshua Mitchell <joshua.mitchell@jax.org>
2023-06-04 17:17:58 -0400

    testing pip install

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+with open("pcpfm/__init__.py") as f:
+    exec([x for x in f.readlines() if '__version__' in x][0])
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read()
+
+setup(
+  name='pcpfm',
+  version=__version__,
+  author='Joshua Mitchell',
+  author_email='joshua.mitchell@jax.org',
+  description='LC-MS metabolomics data processing pipeline',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/jmmitc06/PythonCentricPipelineForMetabolomics',
+  license='MIT',
+  keywords='metabolomics bioinformatics mass spectrometry',
+
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Scientific/Engineering :: Chemistry',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ],
+
+  packages=find_packages(
+    include=['*', '']
+  ),
+  #data_files=[ ('asari/db', ['asari/db/mass_indexed_compounds.pickle', 'asari/db/emp_cpds_trees.pickle']) ],
+  #include_package_data=True,
+  #zip_safe=True,
+  entry_points = {
+        'console_scripts': ['pcpfm=pcpfm.main.py'],
+    },
+
+  python_requires='>=3.7',
+  install_requires=requirements,
+)
